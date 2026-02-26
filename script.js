a39b6879cb0680b7dab4b5fc2ee7c7929c87b91c
Pedugol22 <lautaropeduto2002@gmail.com>
2026-02-26 12:25:30 -0300

    Actualizar servicio a Diseño y Desarrollo Web

---
+++ script.js
@@ -0,0 +1,12 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+if (navToggle && navLinks) {
+  navToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+  });
+
+  navLinks.querySelectorAll('a').forEach((link) => {
+    link.addEventListener('click', () => navLinks.classList.remove('show'));
+  });
+}
